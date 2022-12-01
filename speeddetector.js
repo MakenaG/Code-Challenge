2d38292d5c04b6077f73e00575aa9016b7eabdac
Miss Mithika <91524542+MakenaG@users.noreply.github.com>
2022-12-01 18:35:02 +0300

    Update speeddetector.js

--- speeddetector.js
+++ speeddetector.js
@@ -1,9 +1,6 @@
-// challenge 2: Speed Detector
-//function declaration!
-
 function speedCheck(speed) {
     
-    let speed1 = ((speed - 70) / 5);
+    let speed1 = prompt((speed - 70) / 5);
 // nested if  implementation   
     if ((speed <= 70)) {
         console. log("OK");
@@ -15,4 +12,4 @@ function speedCheck(speed) {
             console. log ("License suspended");
         };
     }
-    speedCheck()
+    speedCheck(60)
